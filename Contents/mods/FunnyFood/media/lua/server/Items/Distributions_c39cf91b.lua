34c6181c0816a1c5a52f59dd624e0e49ffcba212
雷佳杰 <jiajie.lei@zhaopin.com.cn>
2023-06-26 17:27:02 +0800

    优化食物参数，新增坤哥食物

--- Contents/mods/FunnyFood/media/lua/server/Items/Distributions_c39cf91b.lua
+++ Contents/mods/FunnyFood/media/lua/server/Items/Distributions_c39cf91b.lua
@@ -2,6 +2,71 @@ Distributions = Distributions or {};
 
 local distributionTable = 
 {
+	Bag_SurvivorBag = 
+	{
+		items = 
+		{
+			"Base.choudoufucan",10,
+			"Base.chouluxiacan",10,
+			"Base.furucan",10,
+			"Base.jiucaihuacan",10,
+			"Base.laoganmacan",10,
+			"Base.zhacaipack",10,
+			"Base.maotai",8,
+		},
+	},
+	GroceryBag1 = 
+	{
+		items = 
+		{
+			"Base.qiepiantusi",20,
+		},
+	},
+	GroceryBag3 = 
+	{
+		items = 
+		{
+			"Base.qiepiantusi",10,
+		},
+	},
+	Paperbag_Spiffos = 
+	{
+		items = 
+		{
+			"Base.mizhihanbao",50,
+			"Base.xiaohanbao",100,
+		},
+	},
+	SurvivorCache1 = 
+	{
+		SurvivorCrate = 
+		{
+			items = 
+			{
+				"Base.choudoufucan",8,
+				"Base.chouluxiacan",10,
+				"Base.furucan",8,
+				"Base.jiucaihuacan",10,
+				"Base.laoganmacan",8,
+				"Base.zhacaipack",10,
+			},
+		},
+	},
+	SurvivorCache2 = 
+	{
+		SurvivorCrate = 
+		{
+			items = 
+			{
+				"Base.choudoufucan",8,
+				"Base.chouluxiacan",10,
+				"Base.furucan",8,
+				"Base.jiucaihuacan",10,
+				"Base.laoganmacan",8,
+				"Base.zhacaipack",10,
+			},
+		},
+	},
 }
 
 table.insert(Distributions, distributionTable);
